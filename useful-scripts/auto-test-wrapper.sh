f0d5f4b5ae67367835196df43aad998128480a97
Wenyu Wang <i@wenyu.io>
2021-09-19 01:16:56 -0500

    sync

--- useful-scripts/auto-test-wrapper.sh
+++ useful-scripts/auto-test-wrapper.sh
@@ -10,18 +10,16 @@ export LOGIN_PY_SCRIPT="$5"
 
 TRACE_ID="${TOOL_ID}-${APP_ID}-${REP}"
 export OUT_DIR="../test-logs/${TRACE_ID}"
-export SCREEN_OUT_DIR="../test-logs/${TRACE_ID}"
+export SCREEN_OUT_DIR="../test-log-screens/${TRACE_ID}"
 
 if [ -d "$OUT_DIR" ]; then
 	echo "It seems that $OUT_DIR is already there. Not overriding at the moment.."
 	exit 1
 fi
 mkdir -p "$OUT_DIR"
-chmod 775 "$OUT_DIR"
 
 # Assume that screen output folder already belongs to us.
 rm -rf "$SCREEN_OUT_DIR"
 mkdir -p "$SCREEN_OUT_DIR"
-chmod 775 "$SCREEN_OUT_DIR"
 
 screen -dmS "main-${TRACE_ID}" bash auto-test-app.sh
